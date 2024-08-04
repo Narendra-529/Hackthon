e94a32e18c147b3315b58b09fe30b02cb3e2b85c
narendra.balaga <narendra.balaga@innovapptive.com>
2024-08-04 13:32:21 +0530

    token fix

--- server/notification/index.js
+++ server/notification/index.js
@@ -8,7 +8,7 @@ admin.initializeApp({
 
 
 
-const Token ="dxBFf3VxRzi84wsAJBpGNg:APA91bFuLpHQxV-kbVNrW9ylP0-v1kANAV-V0xYzcV3Y7tW1n_JdNmmjZ_vvNPzcUkzGWKgTP6EEx6od_WPjI-h792PUiIZ0WKiovmd3HSssgfUfr3O_AV5wtH-cSqqOIK8WfaVTU8Qr"
+const Token = process.env.TOKEN || "dxBFf3VxRzi84wsAJBpGNg:APA91bFuLpHQxV-kbVNrW9ylP0-v1kANAV-V0xYzcV3Y7tW1n_JdNmmjZ_vvNPzcUkzGWKgTP6EEx6od_WPjI-h792PUiIZ0WKiovmd3HSssgfUfr3O_AV5wtH-cSqqOIK8WfaVTU8Qr"
 const sendNotification = (message, token = Token) => {
   const payload = {
     notification: {
